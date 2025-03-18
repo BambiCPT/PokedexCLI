e7c01559b33c4436485ac059278bba9c5d3a9e0c
Dean <deanobambi@gmail.com>
2025-03-18 11:54:57 +0200

    Built the REPL logic support

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BambiCPT/pokedexcli
+
+go 1.24.0
---
+++ repl.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Provides list of commands",
+		callback:    commandHelp,
+	},
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+
+		if cmd, exists := commands[input[0]]; exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	loweredText := strings.ToLower(text)
+	cleanOutput := strings.Fields(loweredText)
+	return cleanOutput
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
